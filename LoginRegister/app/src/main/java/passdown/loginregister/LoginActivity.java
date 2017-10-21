50d25b7e7ef0278a34e56a6dd7ce750a7647b19e
Khang <khn1@students.uwf.edu>
2017-10-21 00:10:46 -0500

    initial commit

---
+++ LoginRegister/app/src/main/java/passdown/loginregister/LoginActivity.java
@@ -0,0 +1,28 @@
+package passdown.loginregister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+        final TextView tvCreateAccount = (TextView) findViewById(R.id.tvCreateAccount);
+
+        tvCreateAccount.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+    }
+}
